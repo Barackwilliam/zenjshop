9226a31e2c573a073ddba9262ee1836f0fb9fa46
barackwilliam <barackwilliam12@gmail.com>
2026-04-01 11:04:48 +0300

    first commit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.zenjshop.app"
+    namespace = "com.example.zenjshop"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -20,7 +20,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.zenjshop.app"
+        applicationId = "com.example.zenjshop"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
